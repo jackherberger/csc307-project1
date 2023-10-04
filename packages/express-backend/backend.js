c34fb3e414453763525ec101a692f2a7a61627d5
jackherberger <jherberg@calpoly.edu>
2023-10-04 14:28:42 -0700

    "Backend and Frontend talking."

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,5 +1,7 @@
 // backend.js
 import express from "express";
+import cors from "cors";
+
 
 const users = { 
     users_list : [
@@ -34,6 +36,7 @@ const users = {
 const app = express();
 const port = 8000;
 
+app.use(cors());
 app.use(express.json());
 
 
