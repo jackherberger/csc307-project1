7b8b6c8f913ba7d7bfd9ebb7608f555d6afe8a3d
jackherberger <jherberg@calpoly.edu>
2023-10-04 14:34:27 -0700

    "Got add user talking with backend"

--- packages/react-frontend/src/MyApp.js
+++ packages/react-frontend/src/MyApp.js
@@ -15,9 +15,13 @@ function MyApp() {
     setCharacters(updated);
   }
 
-  function updateList(person) {
-    setCharacters([...characters, person]);
-  }
+  function updateList(person) { 
+    postUser(person)
+      .then(() => setCharacters([...characters, person]))
+      .catch((error) => {
+        console.log(error);
+      })
+}
 
   function fetchUsers() {
     const promise = fetch("http://localhost:8000/users");
@@ -30,6 +34,18 @@ function MyApp() {
       .then((json) => setCharacters(json["users_list"]))
       .catch((error) => { console.log(error); });
   }, [] );
+
+  function postUser(person) {
+    const promise = fetch("Http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(person),
+    });
+
+    return promise;
+  }
 
 
   return (
